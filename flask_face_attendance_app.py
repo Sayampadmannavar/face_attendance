43ba6472865d6b2fd4dafa9eca9a3992952a381b
sayam <sayampadmannavar2@gmail.com>
2025-11-17 22:42:27 +0530

    Prepare repo for Render: add .gitignore, trainer, env-driven config

--- flask_face_attendance_app.py
+++ flask_face_attendance_app.py
@@ -409,10 +409,7 @@ def health():
 # Run server
 # ---------------------
 
-if __name__ == '__main__':
-    print('Starting Flask dashboard on http://127.0.0.1:5000')
-
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
